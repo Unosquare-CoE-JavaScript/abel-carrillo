7831a769b9da0028b9c276c8ff334d5dcd1ef417
Abel Carrillo <abel.carrillo@unosquare.com>
2021-09-05 14:48:37 -0500

    Small correction - recieve each bookName in addFavoriteBook

--- 01-programming-primer/solution.js
+++ 01-programming-primer/solution.js
@@ -1,18 +1,19 @@
 // book list
-var books = [
+/* var books = [
     "A Song of Ice and Fire",
     "The Great Gatsby",
     "Crime & Punishment",
     "Great Expectations",
     "You Don't Know JS"
-];
+]; */
 
 // favorite books list
 var favoriteBooks = [];
 
 // TODO: define addFavoriteBook(..) function
-const addFavoriteBook = () => {
-    books.map( book => !book.includes('Great') ? favoriteBooks.push( book ) : '' );
+const addFavoriteBook = ( bookName ) => {
+    //  books.map( book => !book.includes('Great') ? favoriteBooks.push( book ) : '' );
+    !bookName.includes('Great') ? favoriteBooks.push( bookName ) : '';
 };
 
 // TODO: define printFavoriteBooks() function
@@ -20,6 +21,10 @@ const printFavoriteBooks = () => {
     console.log( `Favorite Books: ${favoriteBooks.length}\n\n${[...favoriteBooks].join('\n')}` );
 };
 
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
 // TODO: print out favorite books
-addFavoriteBook();
 printFavoriteBooks();
